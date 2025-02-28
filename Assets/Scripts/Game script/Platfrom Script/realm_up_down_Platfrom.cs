4c3b52a126df3f9ca868972322f2fb434365fb9e
Sujal1s <pachpandesujal555@gmail.com>
2025-02-28 20:03:18 +0530

    realm_up_down_Platfrom

---
+++ Assets/Scripts/Game script/Platfrom Script/realm_up_down_Platfrom.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class realm_up_down_Platfrom : MonoBehaviour
+{
+    [SerializeField] private float moveDistance = 4.5f;
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Vector3 startPosition;
+    private Vector3 targetPosition;
+    private bool movingup = true;
+    [SerializeField] private RealmShift realmShift;
+    void Start()
+    {
+        startPosition = transform.position;
+        targetPosition = startPosition + new Vector3(0, moveDistance, 0);
+    }
+
+    void Update()
+    {
+        if (movingup&& CanUseAbilities())
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            if (transform.position == targetPosition && CanUseAbilities())
+            {
+                movingup = false;
+                targetPosition = startPosition - new Vector3(0, moveDistance, 0);
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            if (transform.position == targetPosition && CanUseAbilities())
+            {
+                movingup = true;
+                targetPosition = startPosition + new Vector3(0, moveDistance, 0);
+            }
+        }
+    }
+    private bool CanUseAbilities()
+    {
+        return realmShift != null && realmShift.isRealmShifted;
+    }
+}
