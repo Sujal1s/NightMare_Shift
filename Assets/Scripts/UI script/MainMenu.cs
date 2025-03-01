1855d1d5056bc54ca8cd0d546e90e23cd985da2e
Sujal1s <pachpandesujal555@gmail.com>
2025-03-01 22:54:27 +0530

    Input and URP & main menu fix

--- Assets/Scripts/UI script/MainMenu.cs
+++ Assets/Scripts/UI script/MainMenu.cs
@@ -6,9 +6,9 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+   {
+       SceneManager.LoadScene("level_1");
+   }
     public void QuitGame()
     {
         Debug.Log("Quit!");
