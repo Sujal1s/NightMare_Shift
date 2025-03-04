b9d7c9c43470f41be3bdaf14d087e74aae9877da
Sujal1s <pachpandesujal555@gmail.com>
2025-03-04 19:48:29 +0530

    CheckPoint script revamp

---
+++ Assets/Scripts/Game script/checkpoint/death_collider.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class death_collider : MonoBehaviour
+{
+    [SerializeField] GameObject checkpoint;
+    [SerializeField] Transform respawnPoint;
+    [SerializeField] private checkpoint cp;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && cp.checkpointrange == true)
+        {
+            other.transform.position = checkpoint.transform.position;
+        }
+        else
+        {
+            other.transform.position = respawnPoint.transform.position;
+        }
+    }
+}
