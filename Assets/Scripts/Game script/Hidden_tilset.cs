0b6217c4cbe4e50eb485279ea00a9f92d7232674
Sujal1s <pachpandesujal555@gmail.com>
2025-03-04 19:49:25 +0530

    Hidden Tilset Script

---
+++ Assets/Scripts/Game script/Hidden_tilset.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering.LookDev;
+using UnityEngine;
+
+
+public class Hidden_tilset : MonoBehaviour
+{
+    [SerializeField] GameObject hiddenTilemap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hiddenTilemap.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            hiddenTilemap.SetActive(false);
+        }
+    }
+
+    /*private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            hiddenTilemap.SetActive(true);
+        }
+    }*/
+}
