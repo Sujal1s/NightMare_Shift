8f66872682813a8dd8756b3c32e433633d5377f5
Sujal1s <pachpandesujal555@gmail.com>
2025-05-06 23:15:21 +0530

    Dialouge box added

--- Assets/Scripts/Game script/miscellaneous Script/ancient murals.cs
+++ Assets/Scripts/Game script/miscellaneous Script/ancient murals.cs
@@ -1,96 +1,132 @@
 using UnityEngine;
-using TMPro;
-
-public class ancient_murals : MonoBehaviour
-{
-    [SerializeField] GameObject promptUI;                // "Press Y" prompt
-    [SerializeField] GameObject dialogueBoxUI;           // Dialogue box panel
-    [SerializeField] TMP_Text dialogueTextUI;            // TextMeshPro text component
-    [SerializeField] string[] dialogueLines;             // Array of dialogue lines
-
-    private int currentIndex = 0;
-    internal bool isNear = false;       
-    private bool isReading = false;
-
-    void Start()
+    using TMPro;
+    using UnityEngine.InputSystem;
+    using System.Collections;
+    
+    public class ancient_murals : MonoBehaviour
     {
-        promptUI.SetActive(false);
-        dialogueBoxUI.SetActive(false);
-        dialogueTextUI.text = "";
-    }
-
-    void Update()
-    {
-        // Only check input if player is near or reading
-        if (isNear || isReading)
+        [SerializeField] GameObject promptUI;
+        [SerializeField] GameObject dialogueBoxUI;
+        [SerializeField] TMP_Text dialogueTextUI;
+        [SerializeField] string[] dialogueLines;
+        [SerializeField] float typingSpeed = 0.05f; // Typing speed in seconds
+    
+        private int currentIndex = 0;
+        internal bool isNear = false;
+        private bool isReading = false;
+    
+        private PlayerInputActions inputActions;
+        private InputAction interactAction;
+        private Coroutine typingCoroutine;
+    
+        void Awake()
         {
-            if (!isReading)
-                promptUI.SetActive(true);
-
-            if (Input.GetButtonDown("PoneglyphRead")) // <-- Controller Y or Keyboard Y
-            {
-                if (!isReading)
-                    StartDialogue();
-                else
-                    NextLine();
-            }
+            inputActions = new PlayerInputActions();
+            interactAction = inputActions.Player.Interact;
         }
-        else
+    
+        void OnEnable()
+        {
+            interactAction.Enable();
+        }
+    
+        void OnDisable()
+        {
+            interactAction.Disable();
+        }
+    
+        void Start()
         {
             promptUI.SetActive(false);
+            dialogueBoxUI.SetActive(false);
+            dialogueTextUI.text = "";
         }
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-            isNear = true;
-    }
-
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
+    
+        void Update()
         {
-            isNear = false;
-            EndDialogueImmediate();
+            if (isNear || isReading)
+            {
+                if (!isReading)
+                    promptUI.SetActive(true);
+    
+                if (interactAction.WasPressedThisFrame())
+                {
+                    if (!isReading)
+                        StartDialogue();
+                    else
+                        NextLine();
+                }
+            }
+            else
+            {
+                promptUI.SetActive(false);
+            }
         }
-    }
-
-    void StartDialogue()
-    {
-        isReading = true;
-        currentIndex = 0;
-        promptUI.SetActive(false);
-        dialogueBoxUI.SetActive(true);
-        dialogueTextUI.text = dialogueLines[currentIndex];
-    }
-
-    void NextLine()
-    {
-        currentIndex++;
-        if (currentIndex < dialogueLines.Length)
+    
+        void OnTriggerEnter2D(Collider2D other)
         {
-            dialogueTextUI.text = dialogueLines[currentIndex];
+            if (other.CompareTag("Player"))
+                isNear = true;
         }
-        else
+    
+        void OnTriggerExit2D(Collider2D other)
         {
-            EndDialogue();
+            if (other.CompareTag("Player"))
+            {
+                isNear = false;
+                EndDialogueImmediate();
+            }
+        }
+    
+        void StartDialogue()
+        {
+            isReading = true;
+            currentIndex = 0;
+            promptUI.SetActive(false);
+            dialogueBoxUI.SetActive(true);
+            if (typingCoroutine != null) StopCoroutine(typingCoroutine);
+            typingCoroutine = StartCoroutine(TypeText(dialogueTextUI, dialogueLines[currentIndex]));
+        }
+    
+        void NextLine()
+        {
+            currentIndex++;
+            if (currentIndex < dialogueLines.Length)
+            {
+                if (typingCoroutine != null) StopCoroutine(typingCoroutine);
+                typingCoroutine = StartCoroutine(TypeText(dialogueTextUI, dialogueLines[currentIndex]));
+            }
+            else
+            {
+                EndDialogue();
+            }
+        }
+    
+        void EndDialogue()
+        {
+            isReading = false;
+            if (typingCoroutine != null) StopCoroutine(typingCoroutine);
+            dialogueTextUI.text = "";
+            dialogueBoxUI.SetActive(false);
+            promptUI.SetActive(false);
+        }
+    
+        void EndDialogueImmediate()
+        {
+            isReading = false;
+            if (typingCoroutine != null) StopCoroutine(typingCoroutine);
+            dialogueTextUI.text = "";
+            dialogueBoxUI.SetActive(false);
+            promptUI.SetActive(false);
+        }
+    
+        private IEnumerator TypeText(TMP_Text textComponent, string textToType)
+        {
+            textComponent.text = "";
+            for (int i = 0; i < textToType.Length; i++)
+            {
+                textComponent.text += textToType[i];
+                yield return new WaitForSeconds(typingSpeed);
+            }
         }
     }
-
-    void EndDialogue()
-    {
-        isReading = false;
-        dialogueTextUI.text = "";
-        dialogueBoxUI.SetActive(false);
-        promptUI.SetActive(false);
-    }
-
-    void EndDialogueImmediate()
-    {
-        isReading = false;
-        dialogueTextUI.text = "";
-        dialogueBoxUI.SetActive(false);
-        promptUI.SetActive(false);
-    }
-}
